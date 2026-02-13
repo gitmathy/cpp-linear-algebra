7c7dfd8942e0edab7f82efaaeb9a52a8b21f5e8a
Oliver <ofortmeier@gmx.de>
2026-02-13 20:37:04 +0100

    wip: Testing resize

--- tests/includes/test_vector.hpp
+++ tests/includes/test_vector.hpp
@@ -2,12 +2,24 @@
 #define TEST_LA_TEST_VECTOR_H
 
 #include "includes/unit_test.hpp"
+#include <sstream>
+#include <typeinfo>
 
 namespace la_test
 {
 
+/// @brief Testing vector
 class vector_test : public unit_test
 {
+private:
+    /// @brief Test constructing a vector
+    template <typename T> bool test_construct();
+
+    /// @brief Test resizing a vector
+    template <typename T> bool test_resize();
+
+    bool test_various_constructs();
+
 public:
     /// @brief Setup the vector test
     vector_test();
@@ -18,6 +30,37 @@ public:
     /// @return True if test succeeded
     bool execute() override;
 };
+
+/// ===============================================
+/// T E M P L A T E   I M P L E M E N T A T I O N S
+/// ===============================================
+
+template <typename T> bool vector_test::test_construct()
+{
+    bool result = true;
+    // construct with defaults
+    la::vector<T> *v = new la::vector<double>(3, T(2));
+    for (::la::size_type i = 0; i < v->size(); ++i)
+    {
+        if (double((*v)(i)-2) > 1e-16)
+        {
+            result = false;
+        }
+    }
+    if (!result)
+    {
+        std::stringstream strs;
+        strs << "vector<" << std::typeid(T()).name() << "> error in constructor";
+        p_errors.push_back(strs.str());
+        p_logger.log(strs.str(), ERROR);
+    }
+    // destruct
+    delete v;
+    return result;
+}
+
+/// @brief Test resizing a vector
+template <typename T> bool vector_test::test_resize() {}
 
 } // namespace la_test
 #endif
--- tests/src/test_vector.cpp
+++ tests/src/test_vector.cpp
@@ -10,25 +10,12 @@ vector_test::vector_test() : unit_test("vector test") {}
 bool vector_test::execute()
 {
     bool result = true;
+    bool construct = true;
+    construct = test_construct<double>() && construct;
+    construct = test_construct<float>() && construct;
+    construct = test_construct<int>() && construct;
 
-    // construct with defaults
-    LA::vector<double> *v = new LA::vector<double>(3, 2.0);
-    bool construct_result = true;
-    for (LA::size_type i = 0; i < v->size(); ++i)
-    {
-        if ((*v)(i)-2.0 > 1e-16)
-        {
-            construct_result = false;
-        }
-    }
-    if (!construct_result)
-    {
-        p_errors.push_back("vector: Invalid value by constructor");
-        p_logger.log("== Invalid value by constructor", ERROR);
-    }
-    // destruct
-    delete v;
-    return result = construct_result;
+    return result;
 }
 
 } // namespace la_test
