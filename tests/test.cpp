d7c84e27da8bb9ec3c9a0eaee644da3174890654
Oliver <ofortmeier@gmx.de>
2026-02-14 11:13:05 +0100

    ci: Testing iterators

--- tests/test.cpp
+++ tests/test.cpp
@@ -4,8 +4,10 @@
 
 #include "includes/test_vector_add_sub_assignment.hpp"
 #include "includes/test_vector_add_sub_ops.hpp"
+#include "includes/test_vector_const_iterator.hpp"
 #include "includes/test_vector_construct.hpp"
 #include "includes/test_vector_copy_assignment.hpp"
+#include "includes/test_vector_iterator.hpp"
 #include "includes/test_vector_move_assignment.hpp"
 #include "includes/test_vector_move_ctor.hpp"
 #include "includes/test_vector_resize.hpp"
@@ -25,6 +27,8 @@ int main()
     tests.transfer(std::make_unique<la_test::vector_move_assignment_test>());
     tests.transfer(std::make_unique<la_test::vector_add_sub_assignment_test>());
     tests.transfer(std::make_unique<la_test::vector_add_sub_ops_test>());
+    tests.transfer(std::make_unique<la_test::vector_iterator_test>());
+    tests.transfer(std::make_unique<la_test::vector_const_iterator_test>());
 
     bool result = tests.run();
     return result ? 0 : 1;
