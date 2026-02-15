a4407b4c036a1c0ec4d43d0c0524a97518f47f6a
Oliver <ofortmeier@gmx.de>
2026-02-15 16:08:07 +0100

    refactor: row-wise and col-wise as type

--- includes/types.hpp
+++ includes/types.hpp
@@ -6,7 +6,15 @@
 namespace la
 {
 
+/// @brief Type used to index vectors and matrices
 typedef std::size_t size_type;
+
+/// @brief Storing matrix row- or column-wise
+enum storage_type
+{
+    ROW_WISE,
+    COLUMN_WISE
+};
 
 } // namespace la
 #endif
