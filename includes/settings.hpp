16ecfb44c858b3d70f61964f01c0d45d509ab4da
Oliver <ofortmeier@gmx.de>
2026-02-15 09:02:52 +0100

    feat: Include settings

---
+++ includes/settings.hpp
@@ -0,0 +1,42 @@
+#ifndef LA_SETTINGS_H
+#define LA_SETTINGS_H
+
+namespace la
+{
+
+/// @brief Check shapes
+#define LA_CHECK_SHAPE_C 1
+
+/// @brief Check boundaries. This has a big impackt on performance
+#define LA_CHECK_BOUNDARY_C 2
+
+/// @brief Check everything
+#define LA_CHECK ~0
+
+// /// @brief Check nothing
+// #define LA_CHECK 0
+
+/// @brief No logs are written
+#define LA_DEBUG_SILENT_C 0
+/// @brief Write logs of severity error
+#define LA_DEBUG_ERROR_C 2
+/// @brief Write logs of severity warning
+#define LA_DEBUG_WARNING_C 4
+/// @brief Write logs of severity info
+#define LA_DEBUG_INFO_C 6
+/// @brief Write logs of severity debug
+#define LA_DEBUG_DEBUG_C 8
+/// @brief Write logs of severity trace
+#define LA_DEBUG_TRACE_C 10
+
+/// @brief Log everything
+#define LA_DEBUG_LEVEL 10
+
+// /// @brief Log nothing
+// #define LA_DEBUG_LEVEL 0
+
+/// @brief Where to write errors and logs
+#define LA_LOG_STREAM std::cout
+
+} // namespace la
+#endif
