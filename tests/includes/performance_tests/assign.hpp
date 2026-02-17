c5c29436e8d1c70b9222ef7ed03cbd39cad31aa0
Oliver <ofortmeier@gmx.de>
2026-02-17 10:44:59 +0100

    feat: Include performance test for assign

--- tests/includes/performance_tests/assign.hpp
+++ tests/includes/performance_tests/assign.hpp
@@ -8,6 +8,18 @@ namespace la
 namespace test
 {
 
+/// @brief Test a=b for vectors
+class vector_assign : public vector_performance_test
+{
+protected:
+    /// @brief Execute a single test
+    inline void run_single_test() override { p_a = p_b; }
+
+public:
+    /// @brief Set me up
+    vector_assign(const size_type runs, const size_type dim) : vector_performance_test("vector_assign", runs, dim) {}
+};
+
 /// @brief Test a+=b for vectors
 class vector_assign_add : public vector_performance_test
 {
@@ -19,6 +31,21 @@ public:
     /// @brief Set me up
     vector_assign_add(const size_type runs, const size_type dim)
         : vector_performance_test("vector_assign_add", runs, dim)
+    {
+    }
+};
+
+/// @brief Test a=b for matrices
+class matrix_assign : public matrix_performance_test
+{
+protected:
+    /// @brief Execute a single test
+    inline void run_single_test() override { p_a = p_b; }
+
+public:
+    /// @brief Set me up
+    matrix_assign(const size_type runs, const size_type m, const size_type n)
+        : matrix_performance_test("matrix_assign", runs, m, n)
     {
     }
 };
