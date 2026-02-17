edc31ae8b6dae6f221a09410112f9a8aceefc0de
Oliver <ofortmeier@gmx.de>
2026-02-17 21:13:48 +0100

    ci: testing matrix*vector

--- test.cpp
+++ test.cpp
@@ -1,3 +1,5 @@
+#include "includes/la.hpp"
+#include "includes/matrix.hpp"
 #include "includes/vector.hpp"
 #include <iostream>
 
@@ -5,6 +7,17 @@ using namespace la;
 
 int main()
 {
-    std::cout << "Testing" << std::endl;
-    vector<double> x(2);
+    matrix<int> A(2, 3);
+    A(0, 0) = 1;
+    A(0, 1) = 2;
+    A(0, 2) = 3;
+    A(1, 0) = 4;
+    A(1, 1) = 5;
+    A(1, 2) = 6;
+    vector<int> x(3);
+    x(0) = 1;
+    x(1) = 2;
+    x(2) = 3;
+    vector<int> y = A * x;
+    std::cout << y(0) << ", " << y(1) << std::endl;
 }
