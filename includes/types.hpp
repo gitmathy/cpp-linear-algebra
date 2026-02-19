4dc258b538a4c81aa805b326506c86981e1883a5
Oliver <ofortmeier@gmx.de>
2026-02-19 21:19:15 +0100

    feat: Include index type for backward for loops

--- includes/types.hpp
+++ includes/types.hpp
@@ -17,6 +17,9 @@ namespace la {
 /// @brief Type used to index vectors and matrices
 typedef std::size_t size_type;
 
+/// @brief Type used to index vectors and matrices backwards, i.e., check >=0
+typedef long int signed_size_type;
+
 /// @brief Storing matrix row- or column-wise
 enum storage_type { ROW_WISE, COLUMN_WISE };
 
