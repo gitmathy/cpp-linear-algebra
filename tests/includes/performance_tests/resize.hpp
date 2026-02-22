d2ad7f4c6b7828aae148c30d4af3895479b07a8d
Oliver <ofortmeier@gmx.de>
2026-02-22 17:01:40 +0100

    feat: New layout of performance tests

--- tests/includes/performance_tests/resize.hpp
+++ tests/includes/performance_tests/resize.hpp
@@ -9,30 +9,39 @@ namespace la {
 namespace test {
 
 /// @brief Test resize a vector (free memory, allocate memory, and assign default value)
-class vector_resize : public vector_performance_test
+class vector_resize : public performance_test
 {
 protected:
     /// @brief Execute a single test
-    inline void run_single_test() override { p_a.resize(p_b.rows()); }
+    inline void run_single_test() override { p_a_vec.resize(p_a_vec.rows() + 1); }
 
 public:
     /// @brief Set me up
-    vector_resize(const size_type runs, const size_type dim)
-        : vector_performance_test("vector_resize", runs, dim)
-    {}
+    vector_resize(const size_type runs) : performance_test("vector_resize", runs) {}
 };
 
 /// @brief Test resize a matrix (free memory, allocate memory, and assign default value)
-class matrix_resize : public matrix_performance_test
+template <storage_type StorageT>
+class matrix_resize : public performance_test
 {
 protected:
     /// @brief Execute a single test
-    inline void run_single_test() override { p_a.resize(p_b.rows(), p_b.cols()); }
+    /// We resize to one larger than the normal size, to enforce resizing and resetting when tearing
+    /// down the test
+    inline void run_single_test() override
+    {
+        if constexpr (StorageT == ROW_WISE) {
+            p_A_row.resize(p_A_row.rows() + 1, p_A_row.cols() + 1);
+        } else {
+            p_A_col.resize(p_A_col.rows() + 1, p_A_col.cols() + 1);
+        }
+    }
 
 public:
     /// @brief Set me up
-    matrix_resize(const size_type runs, const size_type m, const size_type n)
-        : matrix_performance_test("matrix_resize", runs, m, n)
+    matrix_resize(const size_type runs)
+        : performance_test(std::string("matrix_resize ") + (StorageT == ROW_WISE ? "row" : "col"),
+                           runs)
     {}
 };
 
