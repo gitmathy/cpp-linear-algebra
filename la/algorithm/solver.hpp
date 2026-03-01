c0df18415a754e0ffa9ede0bd21004116a703084
Oliver <ofortmeier@gmx.de>
2026-03-01 19:01:34 +0100

    feat: Add more logging

--- la/algorithm/solver.hpp
+++ la/algorithm/solver.hpp
@@ -10,47 +10,6 @@
 #ifndef LA_ALGORITHM_SOLVER_HPP
 #define LA_ALGORITHM_SOLVER_HPP
 namespace la {
-namespace algorithm {
-
-// template <typename T, storage_type StorageT>
-// void lu_decomposition<T, StorageT>::solve(vector<T> &x, const vector<T> &rhs)
-// {
-//     const size_type n = p_a.rows();
-//     if (n != rhs.rows()) {
-//         throw util::error("rhs of wrong size", "algorithm:lup");
-//     }
-//     if (x.rows() != n) {
-//         x.allocate(n);
-//     }
-
-//     // 1. Forward pass
-//     T sum;
-//     for (size_type i = 0; i < n; ++i) {
-//         sum = T(0);
-//         for (size_type j = 0; j < i; ++j) {
-//             sum += p_a(p_p(i), +j) * x(j);
-//         }
-//         x(i) = rhs(p_p(i)) - sum;
-//     }
-
-//     // 2. Backward pass
-//     for (signed_size_type i = signed_size_type(n - 1); i >= 0; --i) {
-//         sum = T(0);
-//         for (size_type j = size_type(i + 1); j < n; ++j) {
-//             sum += p_a(p_p(i), j) * x(j);
-//         }
-//         x(i) = (x(i) - sum) / p_a(p_p(i), i);
-//     }
-// }
-
-// template <typename T, storage_type StorageT>
-// vector<T> lu_decomposition<T, StorageT>::solve(const vector<T> &rhs)
-// {
-//     vector<T> x(0);
-//     solve(x, rhs);
-//     return x;
-// }
-
-} // namespace algorithm
+namespace algorithm {} // namespace algorithm
 } // namespace la
 #endif
