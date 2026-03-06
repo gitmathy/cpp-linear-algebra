48c3248b9c46706bb632f2c66379bd19743a6ae7
Oliver <ofortmeier@gmx.de>
2026-03-06 21:02:41 +0100

    feat: First tests for sparse matrix builder

---
+++ tests/includes/unit_tests/test_sparse_matrix_builder.hpp
@@ -0,0 +1,45 @@
+/// Part of the project "cpp-linear-algebra"
+///
+/// @file tests/includes/unit_tests/test_sparse_matrix_builder.hpp
+/// @brief Definition of testing sparse matrix builder
+/// @author Gitmathy, https://github.com/gitmathy
+///
+/// @copyright Copyright (c) 2026. All rights reserved.
+/// Licensed under the MIT License (see LICENSE file in project root).
+
+#ifndef LA_TEST_INCLUDES_UNIT_TEST_SPARSE_MATRIX_BUILDER_HPP
+#define LA_TEST_INCLUDES_UNIT_TEST_SPARSE_MATRIX_BUILDER_HPP
+
+#include "tests/includes/unit_test.hpp"
+
+namespace la {
+namespace test {
+
+/// @brief Test accessing elements in a sparse matrix builder
+class sparse_matrix_builder_insert_test : public unit_test
+{
+public:
+    /// @brief Constructing the test
+    sparse_matrix_builder_insert_test()
+        : unit_test("sparse_matrix_builder_insert_test", "Test access elements")
+    {}
+
+    /// @brief Run the test
+    int execute();
+};
+
+class sparse_matrix_builder_assemble_test : public unit_test
+{
+public:
+    /// @brief Constructing the test
+    sparse_matrix_builder_assemble_test()
+        : unit_test("sparse_matrix_builder_assemble_test", "Test assemble sparse matrix")
+    {}
+
+    /// @brief Run the test
+    int execute();
+};
+
+} // namespace test
+} // namespace la
+#endif
