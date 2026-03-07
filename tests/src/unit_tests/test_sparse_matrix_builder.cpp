fb626a6287fb8fc8aefb5a84ec3f2fed907aad14
Oliver <ofortmeier@gmx.de>
2026-03-07 09:49:14 +0100

    ci: Include tests for iterators of sparse matrix

--- tests/src/unit_tests/test_sparse_matrix_builder.cpp
+++ tests/src/unit_tests/test_sparse_matrix_builder.cpp
@@ -49,5 +49,30 @@ int sparse_matrix_builder_assemble_test::execute()
     return (int)errors().size();
 }
 
+int sparse_matrix_iterator_test::execute()
+{
+    sparse_matrix_builder<int> a_build(3, 3);
+    a_build(0, 0) = 1;
+    a_build(1, 1) = 1;
+    a_build(2, 2) = 1;
+    a_build(1, 1) += 1;
+    a_build(2, 2) = 3;
+    const sparse_matrix<int> a(std::move(a_build));
+
+    if (size_type(std::distance(a.begin(), a.end())) != 3) {
+        report_error("Wrong number of non-zero values");
+    }
+    if (size_type(std::distance(a.begin_col_idx(), a.end_col_idx())) != 3) {
+        report_error("Wrong number of non-zero column indices");
+    }
+    if (size_type(std::distance(a.begin_row_ptr(), a.end_row_ptr())) != 4) {
+        LOG_ERROR("Number of row pointers " << std::distance(a.begin_row_ptr(), a.end_row_ptr())
+                                            << " not as expected 4");
+        report_error("Wrong number of row pointers");
+    }
+
+    return (int)errors().size();
+}
+
 } // namespace test
 } // namespace la
