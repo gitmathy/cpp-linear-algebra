f179a033b0483b3170aa3806c87fa9966ffc2b3c
gitmathy <oliver.fortmeier@outlook.de>
2026-03-10 17:05:23 +0100

    ci: Initial add of unit tests via google unit test

---
+++ unit_tests/test.cpp
@@ -0,0 +1,26 @@
+#include "pch.h"
+#include <gtest/gtest.h>
+#include <la/data_structure/vector.hpp>
+
+namespace la {
+TEST(vector, default_constructor)
+{
+    vector<int> v;
+    EXPECT_EQ(v.rows(), 0);
+}
+
+TEST(vector, constructor_with_size)
+{
+    vector<int> v(2);
+    EXPECT_EQ(v.rows(), 2);
+    EXPECT_TRUE(v(0) == 0 && v(1) == 0);
+}
+
+TEST(vector, constructor_with_size_and_value)
+{
+    vector<int> v(2, 1);
+    EXPECT_EQ(v.rows(), 2);
+    EXPECT_TRUE(v(0) == 1 && v(1) == 1);
+}
+
+} // namespace la
