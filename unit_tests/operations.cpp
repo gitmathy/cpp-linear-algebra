329c125c001c3b1782844435be5d90b3ba641dae
Oliver <ofortmeier@gmx.de>
2026-03-19 21:05:39 +0100

    ci: Test sparse matrix add

--- unit_tests/operations.cpp
+++ unit_tests/operations.cpp
@@ -16,6 +16,7 @@
 
 namespace la {
 
+/// @brief Testing column index iterator
 TEST(ops, const_col_idx_iterator)
 {
     sparse_matrix<int> a({{0, 1}, {2}, {1, 3}}, {3, 4, 5, 6, 7}, 5);
@@ -42,6 +43,34 @@ TEST(ops, const_col_idx_iterator)
     EXPECT_EQ(*(iter_row2++), 3);
     EXPECT_EQ(*(iter_row2++), 4);
     EXPECT_EQ(iter_row2, iter_row2.end());
+}
+
+/// \brief Testing sparse matrix add
+TEST(ops, sparse_matrix_add)
+{
+    const sparse_matrix<int> a({{0, 1}, {2}, {1, 3}}, {3, 4, 5, 6, 7}, 5);
+    const sparse_matrix<int> b({{1}, {1}, {1, 3, 4}}, {3, 4, 5, 6, 7}, 5);
+    sparse_matrix_builder<int> c = a + b;
+    EXPECT_EQ(c.rows(), 3);
+    EXPECT_EQ(c.cols(), 5);
+
+    EXPECT_EQ(c(0, 0), 3);
+    EXPECT_EQ(c(0, 1), 7);
+    EXPECT_EQ(c(0, 2), 0);
+    EXPECT_EQ(c(0, 3), 0);
+    EXPECT_EQ(c(0, 4), 0);
+
+    EXPECT_EQ(c(1, 0), 0);
+    EXPECT_EQ(c(1, 1), 4);
+    EXPECT_EQ(c(1, 2), 5);
+    EXPECT_EQ(c(1, 3), 0);
+    EXPECT_EQ(c(1, 4), 0);
+
+    EXPECT_EQ(c(2, 0), 0);
+    EXPECT_EQ(c(2, 1), 11);
+    EXPECT_EQ(c(2, 2), 0);
+    EXPECT_EQ(c(2, 3), 13);
+    EXPECT_EQ(c(2, 4), 7);
 }
 
 } // namespace la
