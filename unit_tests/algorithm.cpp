722de47f640fa7931f1e007116774fbca70cbaed
Oliver <ofortmeier@gmx.de>
2026-03-20 20:27:55 +0100

    ci: Include file for algorithm testing

---
+++ unit_tests/algorithm.cpp
@@ -0,0 +1,10 @@
+/// Part of the project "cpp-linear-algebra"
+///
+/// @file unit_tests/algorithm.cpp
+/// @brief Unit tests for algorithms
+/// @author Gitmathy, https://github.com/gitmathy
+///
+/// @copyright Copyright (c) 2026. All rights reserved.
+/// Licensed under the MIT License (see LICENSE file in project root).
+
+// test irregular matrix for lu decomposition
