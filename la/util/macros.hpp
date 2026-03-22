bd897deb3582f6d2beb2042ab415480ada949a81
Oliver <ofortmeier@gmx.de>
2026-03-22 21:03:03 +0100

    feat: Include function name in log

--- la/util/macros.hpp
+++ la/util/macros.hpp
@@ -40,11 +40,20 @@ namespace la {
 /// @brief Where to write errors and logs
 #define LA_LOG_STREAM std::cout
 
+#ifndef __FUNCTION_NAME__
+#    ifdef _MSC_VER // WINDOWS
+#        define __FUNCTION_NAME__ __FUNCTION__
+#    else //*NIX
+#        define __FUNCTION_NAME__ __func__
+#    endif
+#endif
+
 // Logging errors
 #if LA_DEBUG_LEVEL >= LA_DEBUG_ERROR_C
 #    define LOG_ERROR(...)                                                                         \
         do {                                                                                       \
-            LA_LOG_STREAM << "LA ERROR: " << __VA_ARGS__ << std::endl;                             \
+            LA_LOG_STREAM << "LA ERROR (" << __FUNCTION_NAME__ << "): " << __VA_ARGS__             \
+                          << std::endl;                                                            \
         } while (false)
 #else
 #    define LOG_ERROR(a) ((void)0)
@@ -54,7 +63,8 @@ namespace la {
 #if LA_DEBUG_LEVEL >= LA_DEBUG_WARNING_C
 #    define LOG_WARNING(...)                                                                       \
         do {                                                                                       \
-            LA_LOG_STREAM << "LA WARNING: " << __VA_ARGS__ << std::endl;                           \
+            LA_LOG_STREAM << "LA WARNING: (" << __FUNCTION_NAME__ << "): " << __VA_ARGS__          \
+                          << std::endl;                                                            \
         } while (false)
 #else
 #    define LOG_WARNING(a) ((void)0)
@@ -64,7 +74,8 @@ namespace la {
 #if LA_DEBUG_LEVEL >= LA_DEBUG_INFO_C
 #    define LOG_INFO(...)                                                                          \
         do {                                                                                       \
-            LA_LOG_STREAM << "LA INFO: " << __VA_ARGS__ << std::endl;                              \
+            LA_LOG_STREAM << "LA INFO: (" << __FUNCTION_NAME__ << "): " << __VA_ARGS__             \
+                          << std::endl;                                                            \
         } while (false)
 #else
 #    define LOG_INFO(a) ((void)0)
@@ -74,7 +85,8 @@ namespace la {
 #if LA_DEBUG_LEVEL >= LA_DEBUG_DEBUG_C
 #    define LOG_DEBUG(...)                                                                         \
         do {                                                                                       \
-            LA_LOG_STREAM << "LA DEBUG: " << __VA_ARGS__ << std::endl;                             \
+            LA_LOG_STREAM << "LA DEBUG: (" << __FUNCTION_NAME__ << "): " << __VA_ARGS__            \
+                          << std::endl;                                                            \
         } while (false)
 #else
 #    define LOG_DEBUG(a) ((void)0)
@@ -84,7 +96,8 @@ namespace la {
 #if LA_DEBUG_LEVEL >= LA_DEBUG_TRACE_C
 #    define LOG_TRACE(...)                                                                         \
         do {                                                                                       \
-            LA_LOG_STREAM << "LA TRACE: " << __VA_ARGS__ << std::endl;                             \
+            LA_LOG_STREAM << "LA TRACE: (" << __FUNCTION_NAME__ << "): " << __VA_ARGS__            \
+                          << std::endl;                                                            \
         } while (false)
 #else
 #    define LOG_TRACE(a) ((void)0)
@@ -109,14 +122,6 @@ inline void la_assert(AssertionT assertion, const std::string &message,
 }
 
 } // namespace util
-
-#ifndef __FUNCTION_NAME__
-#    ifdef _MSC_VER // WINDOWS
-#        define __FUNCTION_NAME__ __FUNCTION__
-#    else //*NIX
-#        define __FUNCTION_NAME__ __func__
-#    endif
-#endif
 
 /// @brief Asserting shapes
 #if LA_CHECK & LA_CHECK_SHAPE_C
